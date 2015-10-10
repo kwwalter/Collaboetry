6b3c9a2bd148ee60593e7b7aa5254e445a0a98d3
Kevin Walter <kevinwalter0731@gmail.com>
2015-10-10 17:17:59 -0400

    Working on set up for server.js

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
+// requiring all the things
+
 var express = require('express'),
     PORT    = process.env.PORT || 3788,
     server  = express(),
-    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-    dbname   = "change_this_later"
-    mongoose = require('mongoose');
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    ejs = require('ejs'),
+    expressEjsLayouts = require('express-ejs-layouts'),
+    methodOverride = require('method-override');
 
+// setting up mongoose stuff
+
+var MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+    dbname   = "change_this_later",
+    mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+mongoose.set('debug', true);
+
+// set up
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+server.use(morgan('dev'));
+
+server.use(express.static('./public'));
+
+server.use(expressEjsLayouts);
+
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+server.use(methodOverride('_method'));
 
 // specific routes--starting with a test one
 
