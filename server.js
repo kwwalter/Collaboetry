03e9c72e959c306a5cf7ae522b4d9c0f75407cf7
Kevin Walter <kevinwalter0731@gmail.com>
2015-10-10 16:35:03 -0400

    Working on more setup

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ var express = require('express'),
     dbname   = "change_this_later"
     mongoose = require('mongoose');
 
+
+// specific routes--starting with a test one
+
 server.get('/wicked-secret-test', function(req, res){
   res.write("welcome to my craptastic app!");
   res.end();
 });
 
+// server listen and mongoose connect
+
 mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function() {
   console.log("SERVER IS UP ON PORT: ", PORT);
-})
+});
