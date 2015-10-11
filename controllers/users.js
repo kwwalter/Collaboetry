2952ebfce843156ab2d4aa2cd7df301b5d1b12c7
Kevin Walter <kevinwalter0731@gmail.com>
2015-10-11 12:07:43 -0400

    Working on setup for user login

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,24 @@ var express = require('express'),
     router  = express.Router(),
     User    = require('../models/user.js');
 
-// TO-DO: require routes for this router
+// routes for this router
+
+router.get('/signup', function(req, res) {
+  res.render('users/signup');
+});
+
+router.post('/signup', function(req, res) {
+  var newUser = User(req.body.user);
+
+  newUser.save(function(err, user){
+    if (err) {
+      console.log("There was an error saving this user to the database");
+      res.redirect(302, '/signup');
+      res.end();
+    } else {
+
+    }
+  });
+});
 
 module.exports = router;
