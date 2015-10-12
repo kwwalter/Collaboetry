4648bd6281ce5df9d858f418fd99e7feddb38924
Kevin Walter <kevinwalter0731@gmail.com>
2015-10-11 23:44:39 -0400

    Got poems to save to the database

--- controllers/poems.js
+++ controllers/poems.js
@@ -15,6 +15,30 @@ router.get('/post-new-poem', function(req, res) {
   }
 });
 
+// user submits a form for a new poem..
+
+router.post('/post-new-poem', function(req, res, next) {
+  var newPoem = Poem(req.body.poem);
+  newPoem.poetID = req.session.currentUser;
+
+  newPoem.save(function(err, poem) {
+    if (err) {
+      console.log("There was an error saving this poem to the database: \n", err);
+      res.redirect(302, '/poems/post-new-poem-fail');
+      res.end();
+    } else {
+      console.log(poem.title, " successfully saved!");
+      res.redirect(302, '/poems-by-author' + "/" + poem.poetID);
+    }
+  });
+});
+
+// user gets an error posting poem..
+
+router.get('/post-new-poem-fail', function(req, res) {
+  res.render('poems/post-new-poem-fail');
+});
+
 // for a listing of all poems, grouped by author
 
 router.get('/poems-by-author', function(req, res){
@@ -34,7 +58,7 @@ router.get('/poems-by-author', function(req, res){
 // this route will grab all poems by a specific author
 
 router.get('/poems-by-author/:id', function(req, res){
-  User.find( {
+  Poem.find( {
     _id: req.params.id
   }, function(err, foundPoems) {
     if (err) {
@@ -71,14 +95,6 @@ router.get('/poems-by-tag/:tag', function(req, res){
       });
     }
   });
-});
-
-router.get('/new', function(req, res, next) {
-  res.render('poems/post-new-poem');
-});
-
-router.post('/new', function(req, res, next) {
-  // submitting new form here.
 });
 
 router.get('/poem/:id', function(req, res, next){
